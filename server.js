b89ef582e60bd09a280cf45d6a09faac9fab6de5
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-12-23 22:11:02 +0000

    [FIXE] server.js

--- server.js
+++ server.js
@@ -11,20 +11,12 @@ var dotenv = require('dotenv');
 var exphbs = require('express-handlebars');
 var passport = require('passport');
 var Data_file = require('./models/Data_file.js');
-
-// Load environment variables from .env file
 dotenv.load();
-
-// Controllers
 var HomeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var contactController = require('./controllers/contact');
-
-// Passport OAuth strategies
 require('./config/passport');
-
 var app = express();
-
 var hbs = exphbs.create({
   defaultLayout: 'main',
   helpers: {
@@ -39,7 +31,6 @@ var hbs = exphbs.create({
     }
   }
 });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 80);
@@ -47,7 +38,6 @@ app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(formidable());
 app.use(expressValidator());
 app.use(methodOverride('_method'));
 app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
@@ -57,13 +47,9 @@ app.use(passport.session());
 app.use(function(req, res, next) {
   sendMeData_file(req.user, function(data_file) {
     res.locals.user = req.user ? req.user.toJSON() : null;
-    if (data_file) {
-      res.locals.data_file = data_file ? data_file.toJSON() : null;
-    }
     next();
   });
 }); 
-
 function sendMeData_file(user, callback) {
   if (!user) {
       callback();
@@ -76,21 +62,15 @@ function sendMeData_file(user, callback) {
       });
   }
 }
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', HomeController.index);
 app.get('/contact', contactController.contactGet);
 app.post('/contact', contactController.contactPost);
 app.get('/account', userController.ensureAuthenticated, userController.accountGet);
 app.put('/account', userController.ensureAuthenticated, userController.accountPut);
-
-//Formidable
 app.post('/home', userController.ensureAuthenticated, HomeController.homePost);
 app.put('/home', userController.ensureAuthenticated, HomeController.homePut);
 app.delete('/home', userController.ensureAuthenticated, HomeController.homeDelete);
-app.get('/upbrowser', userController.ensureAuthenticated, HomeController.upbrowserGet);
-
 app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
 app.get('/signup', userController.signupGet);
 app.post('/signup', userController.signupPost);
@@ -107,28 +87,17 @@ app.get('/auth/google', passport.authenticate('google', { scope: 'profile email'
 app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }));
-
-app.get('/home', function (req, res)
-{
-   res.senFile(__dirname + '/home'); 
-});
-
-// Production error handler
 if (app.get('env') === 'production') {
   app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.sendStatus(err.status || 500);
   });
 }
-
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.send(404, 'Page introuvable !');
 });
-
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-
 module.exports = app;
