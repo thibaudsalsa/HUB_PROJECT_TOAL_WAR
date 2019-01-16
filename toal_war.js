1b881554a473e3768463bbe6d744d3e18c57eebc
Thibaud salsa <thibaud.salsa@upscribers.org>
2019-01-16 19:59:15 +0000

    [FIXE] websocket

--- toal_war.js
+++ toal_war.js
@@ -2,15 +2,15 @@ var fs = require("fs");
 var vm = require("vm");
 var WebSocketServer = require('ws').Server;
 
-vm.runInThisContext(fs.readFileSync(__dirname + "/fight.js"));
-vm.runInThisContext(fs.readFileSync(__dirname + "/initGame.js"));
-vm.runInThisContext(fs.readFileSync(__dirname + "/card.js"));
-
 var start = false;
 var player_in = [];
 var players = [false, false, false];
 var wss;
 var game;
+vm.runInThisContext(fs.readFileSync(__dirname + "/fight.js"));
+vm.runInThisContext(fs.readFileSync(__dirname + "/initGame.js"));
+vm.runInThisContext(fs.readFileSync(__dirname + "/card.js"));
+
 start_server();
 setInterval(() => refresh_game(), 13);
 
