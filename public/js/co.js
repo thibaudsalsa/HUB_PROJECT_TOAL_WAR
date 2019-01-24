8bc43dae47b71e8afb713290c369250016f555f0
Thibaud salsa <thibaud.salsa@upscribers.org>
2019-01-24 23:02:29 +0000

    [FIXE] txt nation

--- public/js/co.js
+++ public/js/co.js
@@ -330,13 +330,10 @@ function refresh_base(msg)
         document.getElementById("nation_power").disabled = "";;
         save_display_nation = true;
     }
-    if ((msg.argent < msg.price_card) && save_display_card != false)
+    if ((msg.argent < msg.price_card) && save_display_card != false && document.getElementById("carte_1").innerHTML != "" && document.getElementById("carte_2").innerHTML != "" && document.getElementById("carte_3").innerHTML != "")
     {
-        if (document.getElementById("carte_1").innerHTML != "" && document.getElementById("carte_2").innerHTML != "" && document.getElementById("carte_3").innerHTML != "")
-        {
-            document.getElementById("buy_card").disabled = "disabled";
-            save_display_card = false;
-        }
+        document.getElementById("buy_card").disabled = "disabled";
+        save_display_card = false;
     }
     else if (msg.argent >= msg.price_card && save_display_card != true)
     {
